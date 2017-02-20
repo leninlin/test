e962891393c0f5fcbb30b61aa85916bfe5097e15
Mikhail Dronov <mike@dronov.net>
2017-02-20 22:04:50 +0400

    add validation and make layout changes

--- js/hardathon.js
+++ js/hardathon.js
@@ -4,7 +4,23 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+$.validator.setDefaults({
+  debug: true,
+  success: "valid"
+});
+
+$( "#hardathonRegisterForm" ).validate({
+  rules: {
+    hardathonUserEmail: {
+      required: true,
+      email: true
+    },
+    hardathonUsername: {
+      required: true
+    }
+  },
+});
+
 $(function() {
     $('a.move-section-down').bind('click', function(event) {
         $.fn.fullpage.moveSectionDown();
@@ -22,5 +38,29 @@ $(document).ready(function() {
         paddingTop: '10px',
         navigation:true,
         controlArrowColor: '#424242',
+        menu: '#menu',
+	anchors: ['main', 'about', 'faq', 'contacts'],
     });
+});
+
+$("#hardathonRegisterForm").submit(function(event) {
+  event.preventDefault();
+  var hardathonFormData = {'username':$('#hardathonUsername').val(), 'email' : $('#hardathonUserEmail').val()};
+  var form = $( "#hardathonRegisterForm" );
+
+  if (form.valid() === true) {
+  $.ajax({
+            url: "http://localhost:4567/api/v1/register",
+            type: "POST",
+            crossDomain: true,
+            data: hardathonFormData,
+            dataType: "json",
+            success: function (response) {
+                alert(response.status);
+            },
+            error: function (xhr, status) {
+                alert("error");
+            }
+        });
+  }
 });
